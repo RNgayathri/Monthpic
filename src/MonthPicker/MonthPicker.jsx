f5d9b26a3be7a9be64099905787c082cdcbd9818
gayathrin <gayathri@highpeaksw.com>
2019-11-15 18:26:25 +0530

    updated pulgins

--- src/MonthPicker/MonthPicker.jsx
+++ src/MonthPicker/MonthPicker.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
-import TextField from "material-ui/TextField";
+import TextField from '@material-ui/core/TextField';
 import { getMonth, getYear, addMonths, subMonths} from 'date-fns';
 import { TooltipContainer, MonthContainer, Divider, Header, SVG } from "./MonthPickerStyles.jsx";
 import Month from "./Month.jsx";
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+// import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
 
 export const Year = styled.span`
   user-select: none;
